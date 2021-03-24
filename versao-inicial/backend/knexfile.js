9349ed730af4b24614d73412ee7dc275ae2e9925
Vinicus <viniciusalcantarabarros@gmail.com>
2021-03-24 14:47:33 -0300

    Criando arquivo knexfile.js

---
+++ versao-inicial/backend/knexfile.js
@@ -0,0 +1,17 @@
+// Update with your config settings.
+
+module.exports = {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'postgress',
+      password: 'admin123456'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+};
