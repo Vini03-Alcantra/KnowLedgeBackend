3d75b08dd05a3c2f388fba7ed0382332147eeb7b
Vinicus <viniciusalcantarabarros@gmail.com>
2021-06-29 23:50:34 -0300

    Criando component de Categorias

--- versao-inicial/frontend/src/components/admin/CategoryAdmin.vue
+++ versao-inicial/frontend/src/components/admin/CategoryAdmin.vue
@@ -1,11 +1,96 @@
 <template>
     <div class="category-admin">
-        <h1>Categoria Componente</h1>
+        <b-form>
+            <input id="category-id" type="hidden" v-model="category.id">
+            <b-row>
+                <b-col md="6" sm="12">
+                    <b-form-group label="Nome" label-for="category-name">
+                        <b-form-input id="category-name" type="text"
+                            v-model="category.name" required
+                            placeholder="Informe o nome da categoria"
+                        />                        
+                    </b-form-group>
+                    <b-row>
+                        <b-col xs="12">
+                            <b-button variant="primary" v-if="mode === 'save'" @click="save">Salvar</b-button>
+                            <b-button variant="danger" v-if="mode === 'remove'" @click="remove">Excluir</b-button>
+                            <b-button class="ml-2" @click="reset">Cancelar</b-button>
+                        </b-col>
+                    </b-row>
+                </b-col>                                
+            </b-row>
+            <hr>
+            <b-table hover striped :items="categories" :fields="fields">                
+                <template slot="actions" slot-scope="data">
+                    <b-button variant="warning" @click="loadCategories(data.item)" class="mr-2">
+                        <i class="fa fa-pencil"></i>
+                    </b-button>
+                    <b-button variant="danger" @click="loadCategories(data.item, 'remove')">
+                        <i class="fa fa-trash"></i>
+                    </b-button>
+                </template>
+            </b-table>
+        </b-form>
     </div>
 </template>
 
 <script>
+import {baseApiUrl, showError} from '@/global'
+import axios from 'axios'
+
 export default{
-    
+    name: "CategoryAdmin",
+    data: function(){
+        return {
+            mode: 'save',
+            category: {},
+            categories: [],
+            fields: [
+                {key: "id", label: "Código", sortable: true},
+                {key: "name", label: "Nome", sortable: true},                
+                {key: "path", label: "Caminho", sortable: true},
+                {key: "actions", label: "Ações"}
+            ]        
+        }
+    },
+    methods: {
+        loadCategories(){
+            const url = `${baseApiUrl}/categories`;
+            axios.get(url).then(res => {
+                this.categories = res.data
+            })
+        },
+        reset(){
+            this.mode = 'save';
+            this.category = {}
+            this.loadCategories()
+        },
+        save(){
+            const method = this.category.id ? 'put':'post'
+            const id = this.category.id ? `/${this.article.id}` : ''
+            axios[method](`${baseApiUrl}/categories${id}`, this.category)
+                .then(() => {
+                    this.$toasted.global.defaultSuccess()
+                    this.reset()
+                })
+                .catch(showError)
+        },
+        remove(){
+            const id = this.category.id;
+            axios.delete(`${baseApiUrl}/categories/${id}`)
+                .then(() => {
+                    this.$toasted.global.defaultSuccess()
+                    this.reset()
+                })
+                .catch(showError)
+        },
+        loadCategorie(categorie, mode = 'save'){
+            this.mode = mode;
+            this.category = {...categorie}
+        }
+    },
+    mounted(){
+        this.loadCategories()
+    }
 }
 </script>
--- versao-inicial/frontend/src/components/admin/UserAdmin.vue
+++ versao-inicial/frontend/src/components/admin/UserAdmin.vue
@@ -7,6 +7,7 @@
                     <b-form-group label="Nome: " label-for="user-name">
                         <b-form-input id="user-name" type="text"
                             v-model="user.name" required
+                            :readonly="mode === 'remove'"
                             placeholder="Informe o nome de usuário"
                         >
                         </b-form-input>
@@ -16,16 +17,17 @@
                     <b-form-group label="Email: " label-for="user-email">
                         <b-form-input id="user-email" type="text"
                             v-model="user.email" required
+                            :readonly="mode === 'remove'"
                             placeholder="Informe o email de usuário"
                         >
                         </b-form-input>
                     </b-form-group>
                 </b-col>
             </b-row>
-            <b-form-checkbox id="user-admin" v-model="user.admin" class="mt-3 mb-3">
+            <b-form-checkbox id="user-admin" v-show="mode === 'save'" v-model="user.admin" class="mt-3 mb-3">
                 Administrador?
             </b-form-checkbox>
-             <b-row>
+             <b-row v-show="mode === 'save'">
                 <b-col md="6" sm="12">
                     <b-form-group label="Senha: " label-for="user-password">
                         <b-form-input id="user-password" type="password"
@@ -45,9 +47,13 @@
                     </b-form-group>
                 </b-col>
             </b-row>
-            <b-button variant="primary" v-if="mode === 'save'" @click="save">Salvar</b-button> 
-            <b-button variant="danger" v-if="mode === 'remove'" @click="remove">Excluir</b-button>
-            <b-button class="ml-2" @click="reset">Cancelar</b-button>            
+            <b-row>
+                <b-col xs="12">
+                    <b-button variant="primary" v-if="mode === 'save'" @click="save">Salvar</b-button>
+                    <b-button variant="danger" v-if="mode === 'remove'" @click="remove">Excluir</b-button>
+                    <b-button class="ml-2" @click="reset">Cancelar</b-button>
+                </b-col>
+            </b-row>
         </b-form>
         <hr>
         <b-table hover striped :items="users" :fields="fields">
