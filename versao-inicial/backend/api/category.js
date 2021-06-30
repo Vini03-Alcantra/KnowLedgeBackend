61e7446a40a56a4ffe3633ec1c4877f4fa585271
Vinicus <viniciusalcantarabarros@gmail.com>
2021-06-30 18:27:55 -0300

    Corrigindo bug de update

--- versao-inicial/backend/api/category.js
+++ versao-inicial/backend/api/category.js
@@ -2,7 +2,11 @@ module.exports = app => {
     const {existsOrError, notExistsOrError } = app.api.validation;
 
     const save = (req, res) => {
-        const category = {...req.body}
+        const category = {
+            id: req.body.id,
+            name: req.body.name,
+            parentId: req.body.parentId
+        }
         if(req.params.id) category.id = req.params.id;
 
         try {
@@ -14,7 +18,7 @@ module.exports = app => {
         if (category.id) {
             app.db('categories')
                 .update(category)
-                .where({id: respository})
+                .where({id: category.id})
                 .then(_ => res.status(204).send())
                 .catch(err => res.status(500).send(err))
         } else {
